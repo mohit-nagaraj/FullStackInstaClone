a47db05c2ce6dccd4daeed2c3b1670706f97b67a
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-03-30 13:59:48 +0530

    add assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -5,17 +5,20 @@ const AuthLayout = () => {
   const isAuthenticated = false;
 
   return (
-    <div>
+    <>
       {isAuthenticated
         ? (
           <Navigate to="/" />
         )
         : (
-          <section>
-            <Outlet />
-          </section>
+          <>
+            <section className='flex flex-1 justify-center items-center flex-col py-10'>
+              <Outlet />
+            </section>
+            <img className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' src='/assets/images/side-img.png' alt='side-img'/>
+          </>
         )}
-    </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   darkMode: ['class'],
   content: [
@@ -7,64 +10,51 @@ module.exports = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}'
   ],
-  prefix: 'tailwind.config.js',
   theme: {
     container: {
       center: true,
       padding: '2rem',
       screens: {
         '2xl': '1400px'
+
       }
     },
     extend: {
       colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
-        }
+        'primary-500': '#877EFF',
+        'primary-600': '#5D5FEF',
+        'secondary-500': '#FFB620',
+        'off-white': '#D0DFFF',
+        red: '#FF5A5A',
+        'dark-1': '#000000',
+        'dark-2': '#09090A',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#7878A3',
+        'light-4': '#5C5C7B'
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+      screens: {
+        xs: '480px'
+
+      },
+      width: {
+        420: '420px',
+        465: '465px'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+
       },
       keyframes: {
         'accordion-down': {
-          from: { height: '0' },
+          from: { height: 0 },
           to: { height: 'var(--radix-accordion-content-height)' }
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' }
+          to: { height: 0 }
         }
       },
       animation: {
