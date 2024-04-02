722b1d2313dc1a813f8e23d1a36fd2c0b26a2025
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-04-02 19:55:04 +0530

    update
    Signed-off-by: mohit-nagaraj <mohitnagaraj20@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import AuthLayout from './_auth/AuthLayout'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import RootLayout from './_route/RootLayout'
+import { Home } from './_route/pages'
+import './globals.css'
+import { Route, Routes } from 'react-router-dom'
+function App() {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
