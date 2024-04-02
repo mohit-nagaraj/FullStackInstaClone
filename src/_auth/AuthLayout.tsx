de92df4e1a60943df017921841b967c9b98f86b7
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-04-02 20:14:37 +0530

    new stage
    Signed-off-by: mohit-nagaraj <mohitnagaraj20@gmail.com>

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Outlet, Navigate } from 'react-router-dom'
 const AuthLayout = () => {
-  return <div>AuthLayout</div>
+  const isAuth = false
+  return (
+    <>
+      {isAuth ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  )
 }
 
 export default AuthLayout
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const SignupForm = () => {
-  return <div>SignupForm</div>
+  return <Button>Click me</Button>
 }
 
 export default SignupForm
