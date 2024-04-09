ddc26d47991dbbe01e03d4704d42e46fe422a99d
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-04-09 09:46:58 +0530

    added different routes
    Signed-off-by: mohit-nagaraj <mohitnagaraj20@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import AuthLayout from './_auth/AuthLayout'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import RootLayout from './_route/RootLayout'
-import { Home } from './_route/pages'
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from './_route/pages'
 import './globals.css'
 import { Route, Routes } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
@@ -17,6 +27,14 @@ function App() {
         </Route>
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
--- src/_route/RootLayout.tsx
+++ src/_route/RootLayout.tsx
@@ -1,7 +1,20 @@
+import BottomBar from '@/components/ui/shared/BottomBar'
+import LeftSidebar from '@/components/ui/shared/LeftSidebar'
+import Topbar from '@/components/ui/shared/Topbar'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
-  return <div>RootLayout</div>
+  return (
+    <div className="w-full rootlayout">
+      <Topbar />
+      <LeftSidebar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <BottomBar />
+    </div>
+  )
 }
 
 export default RootLayout
