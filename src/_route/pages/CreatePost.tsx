7235ebf945bec203b0e58c115e27176bb4b48086
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-04-21 07:36:32 +0530

    update

--- src/_route/pages/CreatePost.tsx
+++ src/_route/pages/CreatePost.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import PostForm from '@/components/ui/forms/PostForm'
 
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="common-container p-4">
+        <div className="max-w-5xl flex-start g-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="add"
+          />
+          <h2 className="h3-bold md-h2-bold text-left w-full">Create Post</h2>
+        </div>
+
+        <PostForm action="Create" />
+      </div>
+    </div>
+  )
 }
 
 export default CreatePost
